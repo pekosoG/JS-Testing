ad682ffae6a660ca0b8622fcfebb13b816caf43a
Israel Garcia <igarcia@itexico.net>
2017-12-11 16:52:18 -0600

    hooks

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,22 @@
 function AuthController(){
 
-    function isAuthorized(roles, neededRole){
+    var roles;
+
+    function setRoles(role){
+        roles = role;
+    }
+
+    function isAuthorized( neededRole){
         return roles.indexOf(neededRole) >=0;
     }
 
-    function isAuthorizedAsync(roles, neededRole, cb){
+    function isAuthorizedAsync( neededRole, cb){
         setTimeout(
             function(){cb(roles.indexOf(neededRole)>=0)},
             0);
     }
 
-    return {isAuthorized,isAuthorizedAsync};
+    return {isAuthorized,isAuthorizedAsync, setRoles};
 }
 
 module.exports = AuthController();
--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -3,23 +3,30 @@ var assert = require('assert');
 var authController = require('../../controllers/auth.controller');
 
 describe('AuthController',function(){
+
+    beforeEach(function(){
+        console.log('Running before each');
+        authController.setRoles(['user']);
+    })
+
     describe('isAuthorized',function(){
         it('should return false if not authorized',function(){
-            assert.equal(false,authController.isAuthorized(['user'],'admin'));
+            assert.equal(false,authController.isAuthorized('admin'));
         })
         it('should return true if authorized',function(){
-            assert.equal(true,authController.isAuthorized(['user'],'user'));
+            assert.equal(true,authController.isAuthorized('user'));
         })
     })
+
     describe('isAuthorizedAsync',function(){
         it('should return false if not authorized',function(done){
-            authController.isAuthorizedAsync(['user'],'admin',function(isAuth){
+            authController.isAuthorizedAsync('admin',function(isAuth){
                 assert.equal(false,isAuth);
                 done();
             })
         })
         it('should return true if  authorized',function(done){
-            authController.isAuthorizedAsync(['user'],'user',function(isAuth){
+            authController.isAuthorizedAsync('user',function(isAuth){
                 assert.equal(true,isAuth);
                 done();
             })
