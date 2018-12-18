5e1e8337480f6fb4b7fe4a9e4dd72507f09ec02f
Israel Garcia <igarcia@itexico.net>
2018-12-18 10:31:02 -0600

    spying in a function

---
+++ gitchecker/tests/controllers/gitControler.spec.js
@@ -0,0 +1,34 @@
+var rewire = require('rewire');
+
+var GitCtrl = rewire('../../controllers/gitController');
+var gitController = GitCtrl();
+var chai = require('chai');
+var sinon = require('sinon');
+chai.should();
+
+var getUser;
+
+describe('gitControler',function(){
+
+    beforeEach(function(){
+        var gitService = GitCtrl.__get__('gitService');
+        getUser = sinon.spy(gitService,'getUser');
+        GitCtrl.__set__('gitService',gitService);
+    });
+
+    it.only('should get user and repos from git service',function(done){
+        
+        this.timeout(10000);
+        
+        var req = {params:{userId:'pekosoG'}};
+        var res = {json: test};
+
+        function test(user){
+            getUser.getCall(0).args[0].should.equal('pekosoG');
+            getUser.calledOnce.should.be.true;
+            done();
+        }
+
+        gitController.userGet(req,res);
+    })
+}); 
